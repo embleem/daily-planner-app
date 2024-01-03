a1bceacbd3ed1812be26331f3d7f5c2ca63f4fcb
Emma Blencowe <emma.blencowe95@gmail.com>
2024-01-03 17:48:46 +0000

    Added comments to JS

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,21 @@
+// GLOBAL VARIABLES
+// Reference moment.js
 var today = moment();
+// reference the whole task (time and textarea)
 var timeBlockEl = document.querySelector('.container');
 
+// REFERENCES
+// Display the current date and time on the paragraph with an id of "currentDay"
+$("#currentDay").text(today.format("LLLL")); // LLLL shows day of the week, date and time
 
-$("#currentDay").text(today.format("LLLL")); 
-
+// EVENT LISTENER
+// Event listener for saveBtn click
 $('.saveBtn').on('click', function () {
+  // get nearby values of the description in jQuery
+  var textValue = $(this).siblings('.description').val();
+  // get the id attribute of the parent div element
   var timeKey = $(this).parent().attr('id');
 
+  // save in local storage
   localStorage.setItem(timeKey, textValue);
 });
