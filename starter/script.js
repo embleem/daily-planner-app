4f5142e5261971757296860725a42bdb50d87f98
Emma Blencowe <emma.blencowe95@gmail.com>
2024-01-09 15:44:00 +0000

    Found more efficient way to look through past, present, future

--- starter/script.js
+++ starter/script.js
@@ -4,6 +4,9 @@ var today = moment();
 // reference the whole task (time and textarea)
 var timeBlockEl = document.querySelector('.container');
 
+// Get the current hour of the day using the dayjs library.
+const currentHour = dayjs().format('H');
+
 // REFERENCES
 // Display the current date and time on the paragraph with an id of "currentDay"
 $("#currentDay").text(today.format("LLLL")); // LLLL shows day of the week, date and time
@@ -21,43 +24,29 @@ $('.saveBtn').on('click', function () {
 });
 
 // Get item from local storage if any
-$('#hour8 .description').val(localStorage.getItem('hour8'));
-$('#hour9 .description').val(localStorage.getItem('hour9'));
-$('#hour10 .description').val(localStorage.getItem('hour10'));
-$('#hour11 .description').val(localStorage.getItem('hour11'));
-$('#hour12 .description').val(localStorage.getItem('hour12'));
-$('#hour13 .description').val(localStorage.getItem('hour13'));
-$('#hour14 .description').val(localStorage.getItem('hour14'));
-$('#hour15 .description').val(localStorage.getItem('hour15'));
-$('#hour16 .description').val(localStorage.getItem('hour16'));
-$('#hour17 .description').val(localStorage.getItem('hour17'));
+// $('#hour9 .description').val(localStorage.getItem('hour9'));
+// $('#hour10 .description').val(localStorage.getItem('hour10'));
+// $('#hour11 .description').val(localStorage.getItem('hour11'));
+// $('#hour12 .description').val(localStorage.getItem('hour12'));
+// $('#hour13 .description').val(localStorage.getItem('hour13'));
+// $('#hour14 .description').val(localStorage.getItem('hour14'));
+// $('#hour15 .description').val(localStorage.getItem('hour15'));
+// $('#hour16 .description').val(localStorage.getItem('hour16'));
+// $('#hour17 .description').val(localStorage.getItem('hour17'));
+// $('#hour18 .description').val(localStorage.getItem('hour18'));
 
 // Function to track tasks and make them change colors if they are in the past, present or future
-function checkTask() {
-    // get current number of hours
-    var currentHour = today.hours();
-  
+function hourColourChange() {
     // loop over each time block
-    $('.time-block').each(function () {
-      var timeId = parseInt($(this).attr('id').split("hour")[1]);
-  
-      // if the time Id attribute is before the current hour, add the past class
-      if (timeId < currentHour) {
-        $(this).addClass('past');
-      } // if the time Id attribute is equal to the current hour, remove the past and future classes and add the present class
-      else if (timeId === currentHour) {
-        $(this).removeClass('past');
-        $(this).removeClass('future');
-        $(this).addClass('present');
-      } // If the time Id attribute is greater than the current time, remove the past and present classes and add the future class
-      else {
-        $(this).removeClass('past');
-        $(this).removeClass('present');
-        $(this).addClass('future');
+        $('.time-block').each(function() {
+          const hourBlock = parseInt(this.id);
+          $(this).toggleClass('past', hourBlock < currentHour);
+          $(this).toggleClass('present', hourBlock === currentHour);
+          $(this).toggleClass('future', hourBlock > currentHour);
+        });
       }
-    });
-  }
-  
+
+      
   // Call the audit task function
   checkTask();
   
